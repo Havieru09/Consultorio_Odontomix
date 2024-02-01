13731bca9c83d29d40ca16cc6758fb7b2417a77d
Havieru09 <jmurillor2@est.ups.edu.ec>
2024-01-31 22:41:45 -0500

    Se agrega nueva api para las consultas

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -65,6 +65,7 @@ Route::post('envioCorreo', [EnvioCorreoController::class, 'envioCorreo']);
     Route::get('informe_citas', [CitaController::class, 'showCitasFechas']);
     Route::get('informe_consultas', [ConsultaController::class, 'showConsultasFechas']);
     Route::get('informe_citas_condicion', [CitaController::class, 'showCitasCondicion']);
+    Route::get('informe_consultas_condicion', [ConsultaController::class, 'showConsultasCondicion']);
 
 //API EXAMENES
 Route::apiResource('examen_atm', Examen_atmController::class);
