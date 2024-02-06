2577f13383add616662cd4e3d07b2b88c2e2d9d9
Havieru09 <jmurillor2@est.ups.edu.ec>
2024-02-06 00:15:03 -0500

    Se agrega en la api las rutas

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -57,9 +57,9 @@ Route::apiResource('identificacion', IdentificacionController::class);
 Route::apiResource('clientes', ClientesController::class);
 Route::apiResource('pacientes', PacientesController::class);
 Route::apiResource('citas', CitaController::class);
-Route::get('citas_fechas', [CitaController::class, 'showCitasFechas']);
+Route::get('citas_paciente/{ididentificacion}', [CitaController::class, 'showPaciente']);
 Route::apiResource('consultas', ConsultaController::class);
-Route::get('citas_paciente/{idpaciente}', [CitaController::class, 'showPaciente']);
+Route::get('consultas_paciente/{ididentificacion}', [ConsultaController::class, 'showPaciente']);
 Route::apiResource('item', ItemController::class);
 Route::post('envioCorreo', [EnvioCorreoController::class, 'envioCorreo']);
 
