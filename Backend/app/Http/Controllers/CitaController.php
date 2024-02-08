ffe28cb60a633488fa1f5adf4af786d6f0d5806c
Havieru09 <jmurillor2@est.ups.edu.ec>
2024-02-07 23:33:12 -0500

    Se agrega validación para comprobar la fecha y cambiar el estado de la cita a pasado de fecha

--- Backend/app/Http/Controllers/CitaController.php
+++ Backend/app/Http/Controllers/CitaController.php
@@ -17,10 +17,10 @@ class CitaController extends Controller
         $citas = Cita::orderBy('estado_cita', 'asc')->orderBy('fechahora_cita', 'asc')->paginate(6);
 
         foreach ($citas as $cita) {
-            $flimite = Carbon::parse($cita->fechahora_cita)->addHours(1)->addMinutes(30)->addDays(1);
+            $flimite = Carbon::parse($cita->fechahora_cita)->addDay()->addMinutes(30);
             $fecha = Carbon::now();
 
-            if ($fecha->lte($flimite)) {
+            if ($fecha->gt($flimite)) {
                 $cita->estado_cita = 2;
                 $cita->save();
             }
