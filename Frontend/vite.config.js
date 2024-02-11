05e1db28b158aa1671df217ba02930cac5ff22bd
Havieru09 <jmurillor2@est.ups.edu.ec>
2024-02-10 23:35:00 -0500

    Se añade lineas al vite config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Agrupa los módulos de node_modules en un "chunk" separado
+          }
+        }
+      }
+    }
+  }
 })
