8014d2644f083a4ec928f8571a491c661104b425
Havieru09 <jmurillor2@est.ups.edu.ec>
2024-02-15 13:51:25 -0500

    Se añade la hora al momento de enviar el correo de la cita

--- Backend/resources/views/welcome.blade.php
+++ Backend/resources/views/welcome.blade.php
@@ -6,7 +6,6 @@
 
     <title>Laravel</title>
 
-    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
@@ -15,7 +14,7 @@
 <body class="antialiased">
     <h1 class="text-center text-4xl font-semibold">Notificación del Sistema de Agendamiento de Citas del consultorio Odontomix</h1>
     <p class="text-center text-2xl font-semibold">Estimado Sr/Sra. {{$msg['nombre_cliente']}} {{$msg['apellidos_cliente']}}</p>
-    <p class="text-center text-2xl font-semibold">Confirmamos que su cita al odontologo para el paciente {{$msg['nombre_paciente']}} {{$msg['apellidos_paciente']}} para el día {{$msg['dia2']}}, {{$msg['dia']}} {{$msg['mes']}} {{$msg['anio']}}</p>
+    <p class="text-center text-2xl font-semibold">Confirmamos que su cita al odontologo para el paciente {{$msg['nombre_paciente']}} {{$msg['apellidos_paciente']}} para el día {{$msg['dia2']}}, {{$msg['dia']}} {{$msg['mes']}} {{$msg['anio']}} a las {{$msg['hora']}}</p>
     <p class="text-center text-2xl font-semibold">Saludos Cordiales</p>
     <p class="text-center text-2xl font-semibold">Este correo ha sido generado automáticamente por el sistema, por favor no responda al mismo.</p>
     <p class="text-center text-2xl font-semibold">Gracias por usar nuestros servicios</p>
